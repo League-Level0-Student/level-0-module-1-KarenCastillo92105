643efa6b723f2ce1d1f3883a6d0eb2fce1668e02
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-08 11:35:36 -0700

    Added Robot Color Chooser

---
+++ src/RobotColorChooser.java
@@ -0,0 +1,31 @@
+/*    Copyright (c) The League of Amazing Programmers 2013-2017
+*    Level 0
+*/
+
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+import java.awt.Color;
+
+
+public class RobotColorChooser {
+	public static void main(String[] args) {
+
+		//3. ask the user what color they would like the robot to draw
+		
+		//4. use an if/else statement to set the pen color that the user requested
+
+        //5. if the user doesn’t enter anything, choose a random color
+
+        //6. put a loop around your code so that you keep asking the user for more colors & drawing them
+		
+		//2. set the pen width to 10
+		
+	    //1.Create a robot and make it draw a shape (this will take more than one line of code)
+		
+	}
+}
+
+
