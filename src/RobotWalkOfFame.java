66cdb358ebf82cbf034ce5a6f6c88a8e249b856e
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-11 14:31:00 -0700

    Remove solution code

--- src/RobotWalkOfFame.java
+++ src/RobotWalkOfFame.java
@@ -10,23 +10,8 @@ public class RobotWalkOfFame {
 	public static void main(String[] args) {
 
 		// 1. Create a Robot. Set the X position of the Robot so that it starts on the left. 
-Robot r = new Robot("mini");
-r.setSpeed(10);
-r.setX(0);
-r.penDown();
-
-
-for (int j=0;j<10;j++) {
-		// 2. Make the Robot draw a star shape. Hint: 144.
-for (int i=0;i<5;i++) {
-	r.turn(144);
-r.move(50);
-}
-r.penUp();
-r.setAngle(90);
-r.move(50);
-r.penDown();
-}		// 3. Set the size of the star to 30.
+		// 2. Make the Robot draw a star shape. Hint: 144 degrees.
+		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
