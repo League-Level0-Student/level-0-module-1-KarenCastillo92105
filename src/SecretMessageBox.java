0020a904ac4793f42068116413e407d455750c24
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-27 10:48:29 -0700

    Add files via upload

---
+++ src/SecretMessageBox.java
@@ -0,0 +1,33 @@
+//    Copyright (c) The League of Amazing Programmers 2013-2017
+//    Level 0
+
+package conditionals;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Secret Message Box / Secure Messaging System
+ * 
+ * You want to leave a message on one of the Mac computers so that only your friend can read it.
+ * You set up the passcode and the secret message. 
+ * Your friend types in the passcode to retrieve the message.
+ * 
+ */
+
+public class SecretMessageBox {
+
+	// 0. Make a main method and put steps 1-5 inside it
+	
+	// 1. Set the passcode in a String variable
+	
+	// 2. Using a pop-up, ask for a secret message and store it in a variable
+
+	// 3. Ask your friend for the passcode and store it in a variable
+
+	// 4. If the passcode matches, show the secret message
+
+	// 5. If the passcode does not match, pop-up "INTRUDER!!"
+
+	// [optional] 6. Have your friend also enter a username, and make sure it is correct before releasing the secret message.
+
+}
---
+++ src/TheRiddler.java
@@ -0,0 +1,24 @@
+//    Copyright (c) The League of Amazing Programmers 2013-2017
+//    Level 0
+
+package conditionals;
+
+public class TheRiddler {
+
+	public static void main(String[] args) {
+
+		// 1. Make a variable to hold the score
+
+		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+
+		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+
+		// 5. Otherwise, say "wrong" and tell them the answer
+
+		// 6. Add some more riddles
+
+		// 2. Make a pop up to show the score.
+		
+	}
+}
+
