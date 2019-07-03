170aaa0078c6e80d564b9e809782a6c972936cb7
root <root@ip-172-31-21-29.us-west-1.compute.internal>
2019-07-03 13:27:30 -0700

    update from league central

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _07_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,16 +12,30 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+ int Score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String Answer = JOptionPane.showInputDialog(null,"What has hands but can not clap?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (Answer.equals("A Clock")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+	Score++;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null, "Wrong!");
+}
 		// 6. Add some more riddles
 
+String Answer2 = JOptionPane.showInputDialog(null,"What has a head and a tail, but no body?");
+if (Answer2.equals("A Coin")) {
+	JOptionPane.showMessageDialog(null, "Correct!");
+}
+else {
+	JOptionPane.showMessageDialog(null, "Wrong!");
+}
 		// 2. Make a pop up to show the score.
+
+JOptionPane.showMessageDialog(null, Score);
 		
 	}
 }
